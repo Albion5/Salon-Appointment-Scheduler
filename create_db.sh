3db3c2972ca4281f9c08dcede99c41600d7a2294
Allie <alliesafronova@gmail.com>
2024-04-01 19:21:06 +0700

    DB creation

---
+++ create_db.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+USER=allie
+DEFAULT_DB_NAME=postgres
+DB_NAME=$1
+if [[ -z $DB_NAME ]]
+then
+    DB_NAME=salon
+fi
+PSQL="psql --username=$USER --dbname=$DEFAULT_DB_NAME --no-align --tuples-only -c"
+DB_EXISTS=$($PSQL "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'")
+if [[ -z $DB_EXISTS ]]
+then
+    echo "$($PSQL "CREATE DATABASE $DB_NAME")"
+fi
+
+
+PSQL="psql --username=$USER --dbname=$DB_NAME --no-align --tuples-only -f"
+echo "$($PSQL create_db.sql)"
---
+++ create_db.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL, phone VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS appointments (
+    appointment_id SERIAL PRIMARY KEY, customer_id INT NOT NULL, service_id INT NOT NULL, time VARCHAR(50) NOT NULL, FOREIGN KEY (customer_id) REFERENCES customers (customer_id), FOREIGN KEY (service_id) REFERENCES services (service_id)
+);
+
+CREATE TABLE IF NOT EXISTS services (
+    service_id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL
+);
+
+TRUNCATE customers, appointments, services;
+
+INSERT INTO
+    services (name)
+VALUES ('Hair'),
+    ('Facial'),
+    ('Massage'),
+    ('Waxing');
