438c4dc4e8c29b9939975d55333cf885ee1fc2d3
Allie <alliesafronova@gmail.com>
2024-04-01 19:27:44 +0700

    First part

--- create_db.sql
+++ create_db.sql
@@ -1,3 +1,7 @@
+CREATE TABLE IF NOT EXISTS services (
+    service_id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS customers (
     customer_id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL, phone VARCHAR(50) UNIQUE NOT NULL
 );
@@ -6,11 +10,7 @@ CREATE TABLE IF NOT EXISTS appointments (
     appointment_id SERIAL PRIMARY KEY, customer_id INT NOT NULL, service_id INT NOT NULL, time VARCHAR(50) NOT NULL, FOREIGN KEY (customer_id) REFERENCES customers (customer_id), FOREIGN KEY (service_id) REFERENCES services (service_id)
 );
 
-CREATE TABLE IF NOT EXISTS services (
-    service_id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL
-);
-
-TRUNCATE customers, appointments, services;
+TRUNCATE customers, appointments, services RESTART IDENTITY;
 
 INSERT INTO
     services (name)
